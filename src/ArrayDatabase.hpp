54c95fe91c9808c2337ead1667aba91d7aa11afc
daniellicodes <777danielli777@gmail.com>
2022-05-15 19:35:24 -0700

    added simple arraydb outline

---
+++ src/ArrayDatabase.hpp
@@ -0,0 +1,33 @@
+#ifndef ARRAYDATABASE
+#define ARRAYDATABASE
+#include "SortingStrats/SortingStrategy"
+#include <bits/stdc++.h>
+using namespace std;
+
+private:
+    DatabaseNode* array[]
+    int size;
+public:
+    string at(unsigned int index){
+        if(index < 0 || index >= size) return "";
+        return array[index];
+    }
+    
+    unsigned int size(){
+        return size;
+    }
+    
+    void sort(string sortStrat="insertion"){
+        SortingStrategy* strat = nullptr;
+        if(toLower(sortStrat) == "bogo"){
+            strat = new BogoSort(size);
+        }else if(toLower(sortStrat) == "quick"){
+            strat = new QuickSort(size);
+        }else{
+            strat = new InsertionSort(size);
+        }
+        strat->sort(array);
+        delete strat;
+    }
+
+#endif
