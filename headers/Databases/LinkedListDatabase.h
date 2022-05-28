b0d535f8415d49051a43007a3bbc3a7721898da5
daniellicodes <777danielli777@gmail.com>
2022-05-28 14:41:35 -0700

    added code for lldatabase.h

--- headers/Databases/LinkedListDatabase.h
+++ headers/Databases/LinkedListDatabase.h
@@ -1,1 +1,24 @@
-class LinkedListDatabase : public Database {}
+#ifndef __linkedlist_db__
+#define __linkedlist_db__
+
+#include "../Database.h"
+
+class LinkedListDatabase : public Database {
+private:
+    struct LinkedListNode{
+        LinkedListNode* next;
+        DatabaseNode* data;
+        LinkedListNode(DatabaseNode* data, LinkedListNode* next=nullptr): data(data), next(next){}
+    };
+    LinkedListNode* head;
+    LinkedListNode* tail;
+public:
+    string at(int index);
+    void add(const string &context);
+    DatabaseNode* get(const string &context);
+    void set(const string &context);
+    void remove(const string &context);
+
+};
+
+#endif
