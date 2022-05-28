0e324106fb65c33937a3ec46971a8def2381558f
Nisarg Patel <awesomenisarg@gmail.com>
2022-05-28 00:47:22 -0700

    Fixed undefined issue

--- headers/Nodes/JSONNode.h
+++ headers/Nodes/JSONNode.h
@@ -18,11 +18,6 @@ class JSONDatabaseNode : public DatabaseNode {
         Document data;
         string key;
     public:
-        JSONDatabaseNode(const string &key) {
-            this->key = key;
-            data.SetObject();
-        }
-        JSONDatabaseNode();
         void add(const string &context);
         string get(const string &context) const;
         void set(const string &context);
@@ -33,43 +28,3 @@ class JSONDatabaseNode : public DatabaseNode {
 };
 
 #endif
-
-
-// EXAMPLE CONTEXT : "/john/age"
-void JSONDatabaseNode::add(const string &context) {
-    vector<string> params = getAllParameters(context);
-    Pointer(params.at(0).c_str()).Create(data);
-}
-
-
-// EXAMPLE CONTEXT : "/john/age 18"
-void JSONDatabaseNode::set(const string &context) {
-    vector<string> params = getAllParameters(context);
-    Pointer(params.at(0).c_str()).Set(data, params.at(1).c_str());
-}
-
-
-// EXAMPLE CONTEXT : "/john/age"
-string JSONDatabaseNode::get(const string &context) const {
-    vector<string> params = getAllParameters(context);
-    return Pointer(params.at(0).c_str()).Get(data)->GetString();
-}
-
-
-// EXAMPLE CONTEXT : "/john/age"
-void JSONDatabaseNode::remove(const string &context) {
-    vector<string> params = getAllParameters(context);
-    Pointer(params.at(0).c_str()).Erase(data);
-}
-
-
-string JSONDatabaseNode::print() {
-    StringBuffer buffer;
-    Writer<StringBuffer> writer(buffer);
-    data.Accept(writer);
-    return buffer.GetString();
-}
-
-string JSONDatabaseNode::getKey() const {
-    return key;
-}
--- src/nodes/JSONNode.cpp
+++ src/nodes/JSONNode.cpp
@@ -1,2 +1,42 @@
 #include "../../headers/Nodes/JSONNode.h"
 
+
+
+// EXAMPLE CONTEXT : "/john/age"
+void JSONDatabaseNode::add(const string &context) {
+    vector<string> params = getAllParameters(context);
+    Pointer(params.at(0).c_str()).Create(data);
+}
+
+
+// EXAMPLE CONTEXT : "/john/age 18"
+void JSONDatabaseNode::set(const string &context) {
+    vector<string> params = getAllParameters(context);
+    Pointer(params.at(0).c_str()).Set(data, params.at(1).c_str());
+}
+
+
+// EXAMPLE CONTEXT : "/john/age"
+string JSONDatabaseNode::get(const string &context) const {
+    vector<string> params = getAllParameters(context);
+    return Pointer(params.at(0).c_str()).Get(data)->GetString();
+}
+
+
+// EXAMPLE CONTEXT : "/john/age"
+void JSONDatabaseNode::remove(const string &context) {
+    vector<string> params = getAllParameters(context);
+    Pointer(params.at(0).c_str()).Erase(data);
+}
+
+
+string JSONDatabaseNode::print() {
+    StringBuffer buffer;
+    Writer<StringBuffer> writer(buffer);
+    data.Accept(writer);
+    return buffer.GetString();
+}
+
+string JSONDatabaseNode::getKey() const {
+    return key;
+}
