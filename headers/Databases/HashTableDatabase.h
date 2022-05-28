6b63a02f9f29d061921263d396709016acc03b6d
daniellicodes <777danielli777@gmail.com>
2022-05-28 16:52:28 -0700

    added layout for htdb.cpp

--- headers/Databases/HashTableDatabase.h
+++ headers/Databases/HashTableDatabase.h
@@ -13,7 +13,6 @@ public:
     DatabaseNode* get(const string &context);
     void set(const string &context);
     void remove(const string &context);
-
 };
 
 #endif
---
+++ src/HashTableDatabase.cpp
@@ -0,0 +1,44 @@
+#include "../headers/Databases/HashTableDatabase.h"
+#include "Nodes/JSONNode.cpp"
+
+#include <string>
+#include <sstream>
+using namespace std;
+
+
+string HashTableDatabase::at(string key){
+    int cur = 0;
+    return hashTable[key]->print();
+}
+
+//example context add/str
+void HashTableDatabase::add(const string &context){
+    istringstream ss(context);
+    int idx = context.rfind('/');
+    string name = context.substr(idx+1);
+    string temp;
+    ss >> temp >> temp >> temp;
+    if(temp.substr(0,4) == "json"){
+    }
+}
+
+DatabaseNode* HashTableDatabase::get(const string &context){
+    istringstream ss(context);
+    string temp;
+    ss >> temp >> temp >> temp;
+    if(temp.substr(0,4) == "json"){
+
+    }
+    return nullptr;
+}    
+void HashTableDatabase::set(const string &context){
+    //??
+}
+void HashTableDatabase::remove(const string &context){
+    istringstream ss(context);
+    string temp;
+    ss >> temp >> temp >> temp;
+    if(temp.substr(0,4) == "json"){
+        
+    }
+}
