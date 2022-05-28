c64f133c9fcb7887dc9715b14405a74d89089105
Nisarg Patel <awesomenisarg@gmail.com>
2022-05-27 18:05:37 -0700

    Route handler completed

--- src/HTTPServer.cpp
+++ src/HTTPServer.cpp
@@ -1,17 +1,17 @@
-#include "pistache/description.h"
 #include "pistache/endpoint.h"
-#include "pistache/http.h"
+#include <pistache/http.h>
+#include <pistache/router.h>
 
 using namespace Pistache;
-
-
-Rest::Router routes_definer() {
-    Rest::Router router;
-    Routes::Get(router, "/users/all", Routes::bind(&HelloWord, this));
-    return router;
-}
-
+using namespace Rest;
 
 void HelloWord(const Rest::Request& request, Http::ResponseWriter response) {
      response.send(Http::Code::Ok, "Hello, World\n");
 }
+
+Rest::Router routes_definer() {
+    Rest::Router router;
+    Routes::Get(router, "/users/all", Routes::bind(&HelloWord));
+    return router;
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@ using namespace Pistache;
 
 
 int main() {
+    Rest::Router all = routes_definer();
+
+    
     Address addr(Ipv4::any(), Port(9080));
     auto opts = Http::Endpoint::options()
                     .threads(1);
 
     Http::Endpoint server(addr);
     server.init(opts);
-    server.setHandler(Http::make_handler<ServerHandler>());
+    server.setHandler(all.handler());
     server.serve();
 }
