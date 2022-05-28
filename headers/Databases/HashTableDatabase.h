b9ecfc73171d5f5e07e41f850410698ede6fb41b
daniellicodes <777danielli777@gmail.com>
2022-05-28 16:49:50 -0700

    added ht db h

--- headers/Databases/HashTableDatabase.h
+++ headers/Databases/HashTableDatabase.h
@@ -1,1 +1,19 @@
-class HashTableDatabase : public Database {}
+#ifndef __HT_DB__
+#define __HT_DB__
+
+#include "../Database.h"
+#include <unordered_map>
+
+class HashTableDatabase : public Database {
+private:
+    unordered_map<string, DatabaseNode*> hashTable;
+public:
+    string at(string key);
+    void add(const string &context);
+    DatabaseNode* get(const string &context);
+    void set(const string &context);
+    void remove(const string &context);
+
+};
+
+#endif
