0720af91d5515c3ea743049ef488a3538d3dcea0
shadJack127 <shashankpbiju@gmail.com>
2022-05-29 01:04:39 -0700

    Adding database comments

--- headers/Nodes/JSONNode.h
+++ headers/Nodes/JSONNode.h
@@ -20,12 +20,19 @@ class JSONDatabaseNode : public DatabaseNode {
         JSONDatabaseNode(const string &key): DatabaseNode(key) {
             data.SetObject();
         }
+	// Creates a Json data structure of type defined by a key.
         void add(const string &context);
+	// this will add data into the the datastructure
         string get(const string &context) const;
+	// depednding on the data structure, it will use various methods to find the desired value and will return it
         void set(const string &context);
+	//it will ask for an id and data object to store, and then using a hash algorithm store the data in a position within the hash table given by the key.
         void remove(const string &context);
+	// given an id, Using the hash algorithm, it will find the data stored at the location and delete it.
         string print();
+	// this function will print the data structure in its entirity
         string getKey() const;
+	// function will return key value
         
 };
 
