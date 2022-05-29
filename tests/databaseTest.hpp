256dcf1016052b324ff3875bf7f9e3190c11a150
shadJack127 <shashankpbiju@gmail.com>
2022-05-29 15:51:52 -0700

    anged tests

--- tests/databaseTest.hpp
+++ tests/databaseTest.hpp
@@ -28,16 +28,6 @@ TEST(ARRAYDBTest, createStringNode) {
 // NOW TEST LINKEDLIST DATABASE
 // MAKE SURE TESTING IS DONE WITH DIFFERENT DATABASES IE. TEST3, TEST4, TEST5
 
-TEST(ARRAYDBTest, createJSONNode) {
-   manager->createDatabase("test", "array");
-   manager->getDatabase("test")->add("json test {\"key\":\"value\"}");
-   EXPECT_EQ("{\"key\":\"value\"}", manager->getDatabase("test")->get("test")->print());
-}
-TEST(ARRAYDBTest, createStringNode) {
-   manager->createDatabase("test2", "array");
-   manager->getDatabase("test2")->add("string test wassup");
-   EXPECT_EQ("wassup", manager->getDatabase("test2")->get("test")->print());
-}
 
 TEST(HASHDBTest, createStringNode) {
    manager->createDatabase("test3", "hashTable");
@@ -62,20 +52,26 @@ TEST(LinkedDBTest, createJSONNode) {
    EXPECT_EQ("{\"key\":\"value\"}", manager->getDatabase("test")->get("test")->print());
 }
 
-
-TEST(ARRAYDBTest, returnSize) {
-   manager->createDatabase("testSize", "array");
-   manager->getDatabase("test3")->add("pos 1");
-   manager->getDatabase("test3")->add("pos 2");
-   manager->getDatabase("test3")->add("pos 3");
-   EXPECT_EQ( 3, manager->getDatabase("test3")->size()->print());
-}
-TEST(LinkedDBTest, returnAt) {
-   manager->createDatabase("testSize", "linkedList");
-   manager->getDatabase("test2")->add("pos 0");
-   manager->getDatabase("test2")->add("pos 1");
-   manager->getDatabase("test2")->add("pos 2");
-   EXPECT_EQ( 2, manager->getDatabase("test2")->at(1)->print());
-}
+// TEST(ARRAYDBTest, returnSize) {
+// //    manager->createDatabase("testSize", "array");
+// //    manager->getDatabase("test3")->add("pos 1");
+// //    manager->getDatabase("test3")->add("pos 2");
+// //    manager->getDatabase("test3")->add("pos 3");
+// //    EXPECT_EQ( 3, manager->getDatabase("test3")->size()->print());
+// // }
+//
+// // TEST(LinkedDBTest, returnAt) {
+// //    manager->createDatabase("testSize", "linkedList");
+// //    manager->getDatabase("test2")->add("pos 0");
+// //    manager->getDatabase("test2")->add("pos 1");
+// //    manager->getDatabase("test2")->add("pos 2");
+// //    EXPECT_EQ( 2, manager->getDatabase("test2")->at(1)->print());
+// // }
+//
+// // TEST(HASHDBTest, createStringNode) {
+// //    manager->createDatabase("test3", "hashTable");
+// //    manager->getDatabase("test3")->add("lol hash test");
+// //    EXPECT_EQ("wassup", manager->getDatabase("test3")->get("test")->print());
+// // }
 
 #endif
