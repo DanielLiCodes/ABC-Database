e0f89d2bf085c686c9a4fa10cbce555a86ab999f
daniellicodes <777danielli777@gmail.com>
2022-05-28 22:38:12 -0700

    done insertion sort

--- headers/SortingStrats/InsertionSort.hpp
+++ headers/SortingStrats/InsertionSort.hpp
@@ -2,14 +2,27 @@
 #define INSERTIONSORT
 
 #include "./SortingStrategy.hpp"
+#include "../Nodes/DatabaseNode.h"
+#include <vector>
 using namespace std;
+
 
 class InsertionSort : public SortingStrategy{
     public:
         InsertionSort(){}
     private:
         void sort(vector<DatabaseNode*> &arr){
-            //add sort code here
+            for(int i = 0; i < arr.size(); i++){
+                int smallest = i;
+                for(int j = i+1; j < arr.size(); j++){
+                    if(arr[j]->print() < arr[smallest]->print()){
+                        smallest = j;
+                    }
+                }
+                DatabaseNode* temp = arr[i];
+                arr[i] = arr[smallest];
+                arr[smallest] = temp;
+            }
         }
 };
 
