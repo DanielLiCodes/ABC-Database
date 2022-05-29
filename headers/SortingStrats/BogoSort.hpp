1e08c8d7966610970a16f88d28f6014922b31327
daniellicodes <777danielli777@gmail.com>
2022-05-28 22:46:01 -0700

    added bogo sort 😹

--- headers/SortingStrats/BogoSort.hpp
+++ headers/SortingStrats/BogoSort.hpp
@@ -2,14 +2,37 @@
 #define BOGOSORT
 
 #include "./SortingStrategy.hpp"
+#include <stdlib.h>
+#include "../Nodes/DatabaseNode.h"
+#include <vector>
 using namespace std;
 
 class BogoSort : public SortingStrategy{
     public:
         BogoSort(unsigned int size){}
     private:
+        void randomShuffle(vector<DatabaseNode*> arr){
+            for(int i = 0; i < arr.size(); i++){
+                int randd = rand() % arr.size();
+                DatabaseNode* temp = arr[i];
+                arr[i] = arr[randd];
+                arr[randd] = temp;
+            }
+        }
+
+        bool inOrder(vector<DatabaseNode*> &arr){
+            for(int i = 1; i < arr.size(); i++){
+                if(arr[i]->print() < arr[i-1]->print()){
+                    return false;
+                }
+            }
+            return true;
+        }
+
         void sort(vector<DatabaseNode*> &arr){
-            //add sort code here
+            while(!inOrder(arr)){
+                randomShuffle(arr);
+            }
         }
 };
 
