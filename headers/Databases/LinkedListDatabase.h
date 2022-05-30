f29cc2460a0b7cfe68b266896396805846a93a14
Daniel Li <64798216+DanielLiCodes@users.noreply.github.com>
2022-05-30 14:30:35 -0700

    Update LinkedListDatabase.h

--- headers/Databases/LinkedListDatabase.h
+++ headers/Databases/LinkedListDatabase.h
@@ -15,7 +15,6 @@ private:
 public:
     ~LinkedListDatabase();
     LinkedListDatabase(const string &name) : Database(name) {}
-    ~LinkedListDatabase();
     string at(int index);
     void add(const string &context);
     DatabaseNode* get(const string &context);
