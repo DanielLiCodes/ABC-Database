e3b3ccdca77fd9e55997a2864af499ea179286af
daniellicodes <777danielli777@gmail.com>
2022-05-30 15:26:34 -0700

    fixed qs

--- headers/SortingStrats/QuickSort.hpp
+++ headers/SortingStrats/QuickSort.hpp
@@ -13,29 +13,30 @@ class QuickSort : public SortingStrategy{
     
         //find partition. sort elements before the partition and elements after
         void sort(vector<DatabaseNode*> &arr){
-            int h = arr.size();
-            int l = 0;
-            
+            quickSort(arr, 0, arr.size()-1);
+        }
+
+        void quickSort(vector<DatabaseNode*> &arr, int l, int h){
             if(l < h){
                 int part_in = partition(arr, l, h);
-
-                sort(arr, l, part_in-1);
-                sort(arr, part_in+1, h);
+                quickSort(arr, l, part_in-1);
+                quickSort(arr, part_in+1, h);
             }
         }
+
         //place pivot index in the correct position in the array
         int partition(vector<DatabaseNode*> &arr, int low, int high){
-            DatabaseNode* pivot = arr[high];
-            int i = (low - 1);
+            DatabaseNode* pivot = arr[low];
+            int i = low+1;
 
-            for(int j = low; j <= high-1; j++){
-                if(arr[j] < pivot){
-                    i++;
+            for(int j = low+1; j <= high; j++){
+                if(arr[j]->print() < pivot->print()){
                     swap(arr, i, j);
+                    i++;
                 }
             }
-            swap(arr, i + 1, high);
-            return(i + 1);
+            swap(arr, low, i-1);
+            return(i - 1);
         }
         void swap(vector<DatabaseNode*> &arr, int a, int b){
             DatabaseNode* temp = arr[a];
